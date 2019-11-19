40078e15808e26d67abc5038b1fb776e5f293b9c
Lysak Tatiana <lysak-tatiana2501@ya.ru>
2019-11-19 11:42:48 +0300

    Add prompt library and update version package

---
+++ brain_games/cli.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+import prompt
+    
+def run():
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + name + "!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
 # -*- coding:utf-8 -*-
+from brain_games.cli import run
 
 def greet():
     print("Welcome to the Brain Games!")
 
 def main():
     greet()
+    print()
+    run()
+
 
 if __name__ == "__main__":
     main()
