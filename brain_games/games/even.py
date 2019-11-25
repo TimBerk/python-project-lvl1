dfd0b9c034937eebc7f50641c7c15c1ba837d419
Lysak Tatiana <lysak-tatiana2501@ya.ru>
2019-11-25 13:24:53 +0300

    Fixed bugs and brain_prime

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
 from random import randint
-import prompt
 
 
 MIN_NUM = 1
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,27 +1,32 @@
 # -*- coding:utf-8 -*-
 from random import randint
-import prompt
 
 
 MIN_NUM = 1
 MAX_NUM = 100
-RULE = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
+RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def find_prime(a):
-    b = 2
-    while (a != b):
-        if a % b:
-            b += 1
-        else:
-            return "no"
+def is_even(a):
+    return False if a % 2 else True
 
-    return "yes"
+
+def is_prime(n):
+    if n <= 1 or is_even(n):
+        return False
+    if n == 2:
+        return True
+
+    num = 2
+    while num ** 2 <= n:
+        if n % num == 0:
+            return False
+        num += 1
+    return True
 
 
 def question():
     num = randint(MIN_NUM, MAX_NUM)
-    print(f"Question: {str(num)}")
-    answer = prompt.string('Answer: ')
-    correct = find_prime(num)
-    return answer, correct
+    question = f"Question: {str(num)}"
+    correct = "yes" if is_prime(num) else "no"
+    return question, correct
