6fc5388dcd233903f834ca3fa295bdc66a06aca6
Lysak Tatiana <lysak-tatiana2501@ya.ru>
2019-11-25 14:02:37 +0300

    Fixed brain_progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,11 +11,10 @@ RULE = "What number is missing in the progression?"
 
 
 def build_progression(start_num, end_num, step):
-    list_1 = [i for i in range(start_num, end_num, step)]
-    correct = choice(list_1)
-    k_val = list_1.index(correct)
-    list_2 = [str(x) if key != k_val else "." for key, x in enumerate(list_1)]
-    return correct, " ".join(list_2)
+    list1 = list(range(start_num, end_num, step))
+    correct = choice(list1)
+    list2 = [str(x) if x != correct else "." for x in list1]
+    return str(correct), " ".join(list2)
 
 
 def question():
@@ -23,6 +22,5 @@ def question():
     step = randint(MIN_STEP, MAX_STEP)
     end_num = start_num + step * 10
     correct, progression = build_progression(start_num, end_num, step)
-    print(f"Question: {progression}")
-    answer = prompt.integer('Answer: ')
-    return answer, correct
+    question = f"Question: {progression}"
+    return question, correct
