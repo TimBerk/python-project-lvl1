a6c8900435e745a6a80f796b4e85989d7eb5d4cb
Lysak Tatiana <lysak-tatiana2501@ya.ru>
2019-11-25 13:07:33 +0300

    Fixed brain_even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,12 +5,11 @@ import prompt
 
 MIN_NUM = 1
 MAX_NUM = 100
-RULE = "Answer \"yes\" if number even otherwise answer \"no\"."
+RULE = 'Answer "yes" if number even otherwise answer "no".'
 
 
 def question():
     num = randint(MIN_NUM, MAX_NUM)
-    print("Question: " + str(num))
-    answer = prompt.string('Answer: ')
+    question = "Question: " + str(num)
     correct = "no" if num % 2 else "yes"
-    return answer, correct
+    return question, correct
